7991798ce7acb7d675ef23b75e718a13088d24fe
KohinaTheCat <clarachick11@hotmail.com>
2020-11-10 22:10:27 -0500

    Connected API anc Frontend

--- src/main.js
+++ src/main.js
@@ -1,6 +1,18 @@
 /* global chrome */
-
+var requestOptions = {
+  method: "GET",
+  redirect: "follow",
+  mode: "no-cors",
+};
 chrome.tabs.getSelected(null, (tab) => {
-  console.log(tab.url)
+  console.log(tab.url);
   document.getElementById("url").innerHTML = tab.url;
+
+  fetch("https://summary-api-zzranger.herokuapp.com/query?article=" + tab.url)
+    .then((response) => response.json())
+    .then(
+      (result) =>
+        (document.getElementById("summary").innerHTML = result.summary)
+    )
+    .catch((error) => console.trace(error));
 });
