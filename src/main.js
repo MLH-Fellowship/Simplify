ef4050ffd5520f878c9710a7e39afcb047dcf421
KohinaTheCat <clarachick11@hotmail.com>
2020-11-11 00:44:43 -0500

    commented and formatted code

--- src/main.js
+++ src/main.js
@@ -1,13 +1,17 @@
 /* global chrome */
+
 var requestOptions = {
   method: "GET",
   redirect: "follow",
   mode: "no-cors",
 };
-chrome.tabs.getSelected(null, (tab) => {
-  console.log(tab.url);
-  document.getElementById("url").innerHTML = tab.url;
 
+chrome.tabs.getSelected(null, (tab) => {
+  // gets the current tab that the user is on
+  document.getElementById("url").innerHTML = tab.url;
+  // displays it to the user
+
+  // API call
   fetch("https://summary-api-zzranger.herokuapp.com/query?article=" + tab.url)
     .then((response) => response.json())
     .then(
